70f82b1c380ede700175601f02dc3e791a227adf
Juan Jose Bianchi <juan-josebianchi@hotmail.com>
2023-08-07 18:56:57 -0300

    Cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AuthModule } from './auth_module/auth.module';
 import { PaymentModule } from './payments/payment.module';
+import * as cors from 'cors'; 
 
 @Module({
   imports: [AuthModule, PaymentModule],
-  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cors({
+      origin: 'https://idrd.gov.co',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    })).forRoutes('*');
+  }
+}
